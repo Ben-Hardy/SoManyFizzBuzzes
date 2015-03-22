66d49c899ee035397b4b57be5474ea7237749bbd
Ben-Hardy <hardybe92@gmail.com>
2015-03-22 11:07:47 -0600

    Added Scala implementation and entry in the readme.

---
+++ FizzBuzz.scala
@@ -0,0 +1,14 @@
+object FizzBuzz {
+	def fizzBuzz(fizz : Int, buzz : Int, num : Int) : String = 
+		if (num % fizz == 0 && num % buzz == 0) "FizzBuzz"
+		else if (num % fizz == 0) "Fizz"
+		else if (num % buzz == 0) "Buzz"
+		else num + ""
+
+	def main(args: Array[String]) {
+		val a = (1 to 10).toList;
+		for ( item <- 1 to 100) {
+			println(fizzBuzz(3, 5, item));
+		}
+	}
+}
