8951ec48fd98dae86d46b71a90e3e0a31580595b
Ben-Hardy <hardybe92@gmail.com>
2015-04-15 10:32:09 -0600

    Fixed MATLAB version. Cell arrays are annoying so I did a simpler version of it.

--- fizzbuzz.m
+++ fizzbuzz.m
@@ -2,16 +2,14 @@ function [ res ] = Untitled( fizz, buzz, num )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-for i=1:num
-	if mod(num, fizz) == 0 && mod(num, buzz) == 0
-		res(i,:) = 'FizzBuzz';
-	elseif mod(num, fizz) == 0
-		res(i,:) = 'Fizz';
-	elseif mod(num, buzz) == 0
-		res(i,:) =  'Buzz';
-	else
-		res(i,:) =  num2str(num);
+if mod(num, fizz) == 0 && mod(num, buzz) == 0
+	res = 'FizzBuzz';
+elseif mod(num, fizz) == 0
+	res = 'Fizz';
+elseif mod(num, buzz) == 0
+	res =  'Buzz';
+else
+	res =  num2str(num);
 	end
-end
 end
 
